95243f2e21548aa89bd2752830474ad4f97cd1e2
Hector Sosa <hectorsosa@Hectors-MBP.home>
2022-06-04 16:48:53 +0200

    Fixing greet API

--- pages/api/greet.ts
+++ pages/api/greet.ts
@@ -4,17 +4,14 @@ type Data = {
     response: string;
 };
 
-export default function handler(
+export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
     if (req.method === "POST") {
         console.log(req.body);
         var myHeaders = new Headers();
-        myHeaders.append(
-            "Authorization",
-            `Bearer ${process.env.BOT_TOKEN}`
-        );
+        myHeaders.append("Authorization", `Bearer ${process.env.BOT_TOKEN}`);
         myHeaders.append("Content-type", "application/json;charset=utf-8");
 
         var raw = `{"channel":"#software-development","text": "Hello!"}`;
@@ -25,10 +22,15 @@ export default function handler(
             body: raw,
         };
 
-        return fetch("https://slack.com/api/chat.postMessage", requestOptions)
-            .then((response) => response.text())
-            .then((result) => console.log(result))
-            .catch((error) => console.log("error", error));
+        try {
+            await fetch(
+                "https://slack.com/api/chat.postMessage",
+                requestOptions
+            );
+        } catch (error) {
+            console.log(error);
+        }
+        res.status(200);
     } else {
         res.status(200).json({ response: "Hello! I'm The Garage" });
     }
