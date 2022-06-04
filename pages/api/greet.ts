d1bb1ba71a68ab36b60721319f6a5247cdfe6ceb
Hector Sosa <hectorsosa@Hectors-MBP.home>
2022-06-04 16:54:18 +0200

    Fixing api

--- pages/api/greet.ts
+++ pages/api/greet.ts
@@ -1,36 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-    response: string;
-};
-
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<Data>
+    res: NextApiResponse
 ) {
     if (req.method === "POST") {
         console.log(req.body);
-        var myHeaders = new Headers();
+        const myHeaders = new Headers();
         myHeaders.append("Authorization", `Bearer ${process.env.BOT_TOKEN}`);
         myHeaders.append("Content-type", "application/json;charset=utf-8");
 
-        var raw = `{"channel":"#software-development","text": "Hello!"}`;
+        const raw = `{"channel":"#software-development","text": "Hello!"}`;
 
-        var requestOptions = {
+        const requestOptions = {
             method: "POST",
             headers: myHeaders,
             body: raw,
         };
 
         try {
-            await fetch(
+            const data = await fetch(
                 "https://slack.com/api/chat.postMessage",
                 requestOptions
             );
+            res.status(200).json(data);
         } catch (error) {
             console.log(error);
         }
-        res.status(200);
     } else {
         res.status(200).json({ response: "Hello! I'm The Garage" });
     }
