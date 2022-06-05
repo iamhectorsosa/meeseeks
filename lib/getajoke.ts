e7bb0a5d572c502e6ff799d05592af7582cf99f1
Hector Sosa <hectorsosa@Hectors-MBP.home>
2022-06-05 21:33:14 +0200

    Reorganizing call functions

---
+++ lib/getajoke.ts
@@ -0,0 +1,13 @@
+export default async function getajoke() {
+    try {
+        const res = await fetch("https://icanhazdadjoke.com/", {
+            method: "GET",
+            headers: { Accept: "application/json" },
+        });
+        const data = await res.json();
+        console.log(data.joke);
+        return data.joke;
+    } catch (error) {
+        console.log(error);
+    }
+}
--- pages/api/joke.ts
+++ pages/api/joke.ts
@@ -1,44 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import getajoke from "../../lib/getajoke";
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     if (req.method === "POST") {
-        let joke: string = "";
-
-        try {
-            const jokeResponse = await fetch("https://icanhazdadjoke.com/", {
-                method: "GET",
-                headers: { Accept: "application/json" },
-            });
-            const jokeText = await jokeResponse.json();
-            joke = jokeText.joke;
-        } catch (error) {
-            console.log(error);
-        }
+        let joke: string = await getajoke();
 
         const headers = {
-            Authorization: `Bearer ${process.env.BOT_TOKEN}`,
+            Authorization: `Bearer ${process.env.BOT_USER_TOKEN}`,
             "Content-type": "application/json;charset=utf-8",
         };
 
-        console.log(JSON.stringify(req.body));
-
         const request_body = req.body || {
-            channel_id: "C03J3G756F8",
+            channel_id: "U03JZSV8JHW",
+            user_name: "Dude",
         };
 
-        const { channel_id, channel_name, user_id } = request_body;
-
-        const raw = `{"channel": "${
+        const { channel_id, channel_name, user_id, user_name } = request_body;
+        let raw = `{"channel": "${
             channel_name === "directmessage" ? user_id : channel_id
         }","blocks": [
             {
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": "${joke}"
+                    "text": "Hi, ${user_name}! Here 👉 ${joke}"
                 }
             }
         ], "text": "${joke}"}`;
@@ -50,10 +38,11 @@ export default async function handler(
         };
 
         try {
-            await fetch(
+            const response = await fetch(
                 "https://slack.com/api/chat.postMessage",
                 requestOptions
             );
+            console.log(response);
             res.status(200).end();
         } catch (error) {
             console.log(error);
